76cfbc852a8ea4d394cea44482385d013b16adb3
maianhhbao1234 <dannilo100nam1311a@outlook.com>
2024-05-10 17:26:28 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const average = (arr) => arr.reduce((a, v) => a + v, 0) / arr.length;
+function isSubsequence(s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) i++;
+    j++;
+  }
+  return i === s.length;
+}
